ebc60d1fe83571d5378e8f720f0f7a55f7f75cc8
razumkov <razumkov@e1.ru>
2021-02-27 11:13:43 +0500

    add c

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('login')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('api_token');
             $table->timestamps();
         });
     }
